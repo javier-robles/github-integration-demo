7331d6cdf17556890a80a50b965af95cd87debfa
javier-robles <fjavier.robles@gmail.com>
2020-11-15 20:17:32 +0100

    add webhook

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ console.log("Running webapp...");
 let Express = require('express');
 let app = new Express();
 let port = process.env.PORT || 80;
-app.use(require('body-parser').urlencoded());
+app.use(require('body-parser').urlencoded({extended:true}));
 app.use(require('body-parser').json());
 
 app.use(Express.static('./app'));
@@ -15,3 +15,5 @@ app.listen(port,()=>{
 	//console.log(`Running webhook listener...`);
 	console.log(`App listening on port ${port}`);
 }) ;
+
+require('./webhook.js')(app);
